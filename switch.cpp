51214f1dca7571a574fe7011417f665fe29c6bc7
kaiccp <84818045+kaiccp@users.noreply.github.com>
2021-05-26 13:58:09 -0300

    Update switch.cpp

--- switch.cpp
+++ switch.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+  int var;
+  cout<<"Digite o valor 1";
+  cin>>var;
+  
+  switch(var)
+  {
+    case 10:
+      cout<<"Valor é 10";
+      break;
+    case 15:
+      cout<<"Valor é 11";
+      break;
+    default:
+      cout<<"Não reconhecido";
+  }
+  return 0;
+}
